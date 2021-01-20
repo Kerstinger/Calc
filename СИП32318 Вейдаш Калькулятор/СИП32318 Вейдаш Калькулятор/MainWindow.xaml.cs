4cde8b0347d79de938c88f957299b24e3c01c34f
Kerstinger <55021822+Kerstinger@users.noreply.github.com>
2021-01-20 19:30:42 +0300

    Add files via upload

---
+++ СИП32318 Вейдаш Калькулятор/СИП32318 Вейдаш Калькулятор/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace СИП32318_Вейдаш_Калькулятор
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// by Veydash aka Kerstinger
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Third.Text = (Convert.ToDouble(First.Text) + Convert.ToDouble(Second.Text)).ToString();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Third.Text = (Convert.ToDouble(First.Text) - Convert.ToDouble(Second.Text)).ToString();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Third.Text = (Convert.ToDouble(First.Text) * Convert.ToDouble(Second.Text)).ToString();
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            Third.Text = (Convert.ToDouble(First.Text) / Convert.ToDouble(Second.Text)).ToString();
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            Third.Text = Math.Sin(Convert.ToDouble(First.Text)).ToString();
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            Third.Text = Math.Cos(Convert.ToDouble(First.Text)).ToString();
+        }
+
+        private void Button_Click_6(object sender, RoutedEventArgs e)
+        {
+            Third.Text = Math.Pow(Convert.ToDouble(First.Text), 2).ToString();
+        }
+
+        private void Button_Click_7(object sender, RoutedEventArgs e)
+        {
+            Third.Text = Math.Pow(Convert.ToDouble(First.Text), Convert.ToDouble(Second.Text)).ToString();
+        }
+
+        private void Button_Click_8(object sender, RoutedEventArgs e)
+        {
+            Third.Text = (Convert.ToDouble(First.Text) % Convert.ToDouble(Second.Text)).ToString();
+        }
+
+        private void Button_Click_9(object sender, RoutedEventArgs e)
+        {
+            Third.Text = Math.Log(Convert.ToDouble(First.Text)).ToString();
+        }
+
+        private void Button_Click_10(object sender, RoutedEventArgs e)
+        {
+            Third.Text = "";
+        }
+    }
+}
